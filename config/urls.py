b9467854e85c000144301d094658886fae2e7245
Anton Tumashov <Tumashovster@Gmail.com>
2025-10-02 22:51:37 +0500

    Дополнение: 'urls'

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('products.urls')),
+    path("", include("products.urls")),
     path("products/", include("products.urls")),
     path("users/", include("users.urls")),
 ]
